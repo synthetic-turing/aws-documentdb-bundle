29a55584c13e4591242d6e36b3741667dd01207b
Nitish Kasturia <nitishkasturia@gmail.com>
2023-11-06 10:04:55 -0500

    Added artifact

--- src/_artifacts.tf
+++ src/_artifacts.tf
@@ -1,17 +1,31 @@
-# resource "massdriver_artifact" "<name>" {
-#   field                = "the field in the artifacts schema"
-#   provider_resource_id = "AWS ARN or K8S SelfLink"
-#   name                 = "a contextual name for the artifact"
-#   artifact = jsonencode(
-#     {
-#       # data = {
-#       #   # This should match the aws-rds-arn.json schema file
-#       #   arn = "aws::..."
-#       # }
-#       # specs = {
-#       #   # Any existing spec in ./specs
-#       #   # aws = {}
-#       # }
-#     }
-#   )
-# }
+locals {
+  data_infrastructure = {
+    project_id = aws_docdb_cluster.main_cluster.id
+    cluster_id = aws_docdb_cluster.main_cluster.id
+  }
+  data_authentication = {
+    username = aws_docdb_cluster.main_cluster.master_username
+    password = aws_docdb_cluster.main_cluster.master_password
+    hostname = aws_docdb_cluster.main_cluster.endpoint
+    port     = 27017
+  }
+}
+
+resource "massdriver_artifact" "mongodb" {
+  field                = "mongodb"
+  provider_resource_id = aws_docdb_cluster.main_cluster.arn
+  name                 = "AWS DocumentDB Cluster ${aws_docdb_cluster.main_cluster.cluster_identifier}"
+  artifact = jsonencode(
+    {
+      data = {
+        infrastructure = local.data_infrastructure
+        authentication = local.data_authentication
+      }
+      specs = {
+        mongo = {
+          version = aws_docdb_cluster.main_cluster.engine_version 
+        }
+      }
+    }
+  )
+}
